b4b89dc812cb66dd4829d172b27c819025eb64f4
gthio <gunawanthio@outlook.com>
2016-05-16 05:56:24 +0800

    Creating a Less and AutoPrefixer gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,17 @@ gulp.task('vet', function() {
 		.pipe($.jshint.reporter('fail'));
 });
 
+gulp.task('styles', function(){
+	
+	log('compiling less to CSS');
+	
+	return gulp
+		.src(config.less)
+		.pipe($.less())
+		.pipe($.autoprefixer({browsers: ['last 2 versions', '> 5%']}))
+		.pipe(gulp.dest(config.temp));
+})
+
 function log(msg){
 	if (typeof(msg) === 'object'){
 		for (var item in msg){
