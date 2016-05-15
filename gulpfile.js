1e8ed3de8a7489c5ffcda8e0f3889d9f9b1b10e1
gthio <gunawanthio@outlook.com>
2016-05-16 05:22:55 +0800

    Conditionally displaying the source files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@ var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
 var util = require('gulp-util');
+var gulpprint = require('gulp-print');
+var gulpif = require('gulp-if');
+var args = require('yargs').argv;
 
 gulp.task('vet', function() {
 	
@@ -12,6 +15,7 @@ gulp.task('vet', function() {
 			'./src/**/*.js',
 			'./*.js'
 		])
+		.pipe(gulpif(args.verbose, gulpprint()))
 		.pipe(jscs())
 		.pipe(jshint())
 		.pipe(jshint.reporter('jshint-stylish', {verbose: true}))
