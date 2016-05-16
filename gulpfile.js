689b6b71ef68579a57180a7a71ed6f82c923d8e8
gthio <gunawanthio@outlook.com>
2016-05-16 22:45:05 +0800

    Deleting files in a dependency task - async deletion with callback

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
+var del = require('del');
+
 var config = require('./gulp.config')();
 
 var $ = require('gulp-load-plugins')({lazy: true});
@@ -17,7 +19,7 @@ gulp.task('vet', function() {
 		.pipe($.jshint.reporter('fail'));
 });
 
-gulp.task('styles', function(){
+gulp.task('styles', ['clean-styles'], function(){
 	
 	log('compiling less to CSS');
 	
@@ -26,7 +28,17 @@ gulp.task('styles', function(){
 		.pipe($.less())
 		.pipe($.autoprefixer({browsers: ['last 2 versions', '> 5%']}))
 		.pipe(gulp.dest(config.temp));
-})
+});
+
+gulp.task('clean-styles', function(done){
+	var files = config.temp + '**/*.css';
+	clean(files, done);
+});
+
+function clean(path, done){
+	log('Cleaning: ' + $.util.colors.blue(path));
+	del(path).then(done());
+}
 
 function log(msg){
 	if (typeof(msg) === 'object'){
