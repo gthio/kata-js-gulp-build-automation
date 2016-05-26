980544084b832b0e2e4770f3bbdc7d1cb2eb314e
gthio <gunawanthio@outlook.com>
2016-05-27 05:02:48 +0800

    Minifying and filtering - optimizing js

--- gulpfile.js
+++ gulpfile.js
@@ -134,6 +134,7 @@ gulp.task('optimize', ['inject'], function() {
 	var assets = $.useref.assets({searchPath: './'});
 	var templateCache = config.temp + config.templateCache.file;
 	var cssFilter = $.filter('**/*.css');
+	var jsFilter = $.filter('**/*.js');
 	
 	return gulp
 		.src(config.index)
@@ -144,6 +145,9 @@ gulp.task('optimize', ['inject'], function() {
 		.pipe(cssFilter)	
 		.pipe($.csso())
 		.pipe(cssFilter.restore())		
+		.pipe(jsFilter)
+		.pipe($.uglify())
+		.pipe(jsFilter.restore())
 		.pipe(assets.restore())
 		.pipe($.useref())
 		.pipe(gulp.dest(config.build));
